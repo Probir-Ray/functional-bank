2253e45cf90d0e39426d2d0dc4b2412a7c304e55
Probir-Ray <somironroy.need@gmail.com>
2021-08-17 22:50:57 +0600

    simple bank calculator

---
+++ js/functional-js.js
@@ -0,0 +1,51 @@
+function getInputValue() {
+    const depositInput = document.getElementById('deposit-input');
+    const depositAmountText = depositInput.value;
+    const depositAmount = parseFloat(depositAmountText);
+    depositInput.value = '';    
+    return depositAmount;
+}
+
+
+
+document.getElementById('depositBtn').addEventListener('click', function(){
+    const depositAmount = getInputValue();
+
+    const depositTotal = document.getElementById('deposit-balance');
+    const depositTotalText = depositTotal.innerText;
+    const previousDepositTotal = parseFloat(depositTotalText);
+
+    depositTotal.innerText = previousDepositTotal + depositAmount;
+
+    const balanceTotal = document.getElementById('balance-amount');
+    const balanceTotalText = balanceTotal.innerHTML;
+    const previousBalanceTotal = parseFloat(balanceTotalText);
+
+    balanceTotal.innerText = previousBalanceTotal + depositAmount;
+
+
+    
+
+});
+
+
+document.getElementById('withdrawBtn').addEventListener('click', function() {
+    const withdrawInput = document.getElementById('withdraw-input');
+    const withdrawInputText = withdrawInput.value;
+    const withdrawAmount = parseFloat(withdrawInputText);
+
+    const withdrawBalance = document.querySelector('.withdraw-balance');
+    const previousWithdrawTotalText = withdrawBalance.innerText;
+    const previousWithdrawTotal = parseFloat(previousWithdrawTotalText);
+    
+    withdrawBalance.innerText = previousWithdrawTotal + withdrawAmount;
+
+    const balanceTotal = document.getElementById('balance-amount');
+    const balanceTotalText = balanceTotal.innerHTML;
+    const previousBalanceTotal = parseFloat(balanceTotalText);
+
+    balanceTotal.innerText = previousBalanceTotal - withdrawAmount;
+
+    withdrawInput.value = '';
+
+})
